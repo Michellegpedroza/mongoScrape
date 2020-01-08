e67b5c0a1c25ee3bb4fab9c17c6179cf9585a767
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2020-01-08 11:04:20 -0800

    added the routes

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,13 +1,84 @@
+//routes ~ Articles
 const { Article } = require('../models')
 const axios = require('axios')
-const cheerio = require ('cheerio')
-const db = require('mongojs')
+const cheerio = require('cheerio')
+const db = require('mongojs')('mongoHeadlines')
 
-module.exports= app=>{
-//Get
+module.exports = app => {
+  //Get Saved Articles
+  app.get('/savedHtml/savedArticles', (req, res) => {
+    Article.find({ isSaved: true })
+      .populate('comments')
+      .then(articles => {
+        res.json(articles)
+      })
+      .catch(e => console.error(e))
+  })
 
+  // GET all articles with refresh
+  app.get('/article', (req, res) => {
+    Article.find({ isSaved: false })
+      .populate('comments')
+      .then(articles => {
+        res.json(articles)
+      })
+      .catch(e => console.error(e))
+  })
 
-//Delete
+  // GET all articles after SCRAPING articles
+  app.get('/articles', (req, res) => {
+    axios.get('https://www.npr.org/sections/news/')
+      .then(({ data: html }) => {
+        const $ = cheerio.load(html)
 
+        $('div.item-info').each((i, el) => {
+          const heading = $(el)
+            .children('h2')
+            .text()
+          const summary = $(el)
+            .children('p')
+            .text()
+          const url = $(el)
+            .children('h2')
+            .children('a')
+            .attr('href')
+          db.articles.insert({
+            heading: heading,
+            summary: summary,
+            url: url,
+            isSaved: false
+          })
+        })
+      })
+      .then(() => {
+        Article.find({ isSaved: false })
+          .populate('comments')
+          .then(articles => {
+            res.json(articles)
+          })
+          .catch(e => console.error(e))
+      })
+      .catch(e => console.error(e))
+  })
 
+  // Update article to save
+  app.put('/articles/:id', (req, res) => {
+    Article.updateOne({ _id: req.params.id }, { isSaved: true })
+      .then(() => res.sendStatus(200))
+      .catch(e => console.error(e))
+  })
+
+  // Delete all articles
+  app.delete('/articles', (req, res) => {
+    Article.deleteMany()
+      .then(() => res.sendStatus(200))
+      .catch(e => console.error(e))
+  })
+
+  // Delete article form saved
+  app.delete('/article/:id', (req, res) => {
+    Article.deleteOne({ _id: req.params.id })
+      .then(() => res.sendStatus(200))
+      .catch(e => console.error(e))
+  })
 }
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,13 +1,42 @@
+//routes ~ Comments
 const { Comment, Article } = require('../models')
 
-module.exports = app =>[
-//Get
+module.exports = app => {
+  //Get
+  app.get('/comments/:comment', (req, res) => {
+    console.log(req.params.comment)
+    Comment.find({ parent: req.params.comment })
+      .then(comments => {
+        res.json(comments)
+      })
+      .catch(e => console.error(e))
+  })
 
+  //Post
+  app.post('/comments', (req, res) => {
+    console.log(req.body)
+    Comment.create(req.body)
+      .then(({ _id }) => {
+        Article.updateOne(
+          {
+            _id: req.body.parent
+          },
+          {
+            $push: {
+              comments: _id
+            }
+          }
+        )
+          .then(() => res.sendStatus(200))
+          .catch(e => console.error(e))
+      })
+      .catch(e => console.error(e))
+  })
 
-
-//Post
-
-
-//Delete
-
-]
+  //Delete
+  app.delete('/comments/:comment', (req, res) => {
+    Comment.deleteOne({ _id: req.params.comment })
+      .then(() => res.sendStatus(200))
+      .catch(e => console.error(e))
+  })
+}
