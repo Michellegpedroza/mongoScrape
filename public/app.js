3aede0dbe40903731a3f195b27ed29c085b20aa9
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2020-01-10 14:37:45 -0800

    added styling to cards

--- public/app.js
+++ public/app.js
@@ -1,1 +1,83 @@
 //public ~ app.js
+
+// builds the articles
+let buildArticles = articles => {
+  document.getElementById('articles').innerHTML = ''
+  articles.forEach(article => {
+    let artElem = document.createElement('div')
+    artElem.innerHTML = `
+	<div class="card" style="width: auto;">
+  <div class="card-body">
+  <a href="${article.url}"  target ="_blank" class="btn btn-primary heading" id="nav">${article.heading}</a>
+    <p class="card-text">${article.summary}</p>
+     <a data-id=${article._id} class="btn btn-primary saveArticle">Save Article</a>
+  </div>
+</div>
+		`
+    document.getElementById('articles').append(artElem)
+  })
+}
+
+// shows articles on refresh
+let showArticles = () => {
+  axios.get('article')
+    .then(({ data }) => {
+      buildArticles(data)
+    })
+    .catch(e => console.error(e))
+}
+showArticles()
+
+// Scrape articles
+let scrapeArticles = () => {
+  let empty = document.getElementById('articles').innerHTML === ''
+  if (empty === true) {
+    axios.get('articles')
+      .then(({ data }) => {
+        console.log(data)
+        buildArticles(data)
+      })
+      .then(() => {
+        showArticles()
+      })
+      .catch(e => console.error(e))
+  } else {
+  }
+}
+
+// Delete all Articles
+let deleteArticles = () => {
+  axios.delete('articles')
+    .then(() => {
+      console.log('deleted')
+      document.getElementById('articles').innerHTML = ''
+    })
+    .catch(e => console.error(e))
+}
+
+// Save articles
+let saveArticles = id => {
+  axios.put(`articles/${id}`)
+    .then(() => {
+      console.log('success')
+    })
+    .catch(e => console.log(e))
+}
+
+// EVENT LISTENERS
+document.addEventListener('click', e => {
+  if (e.target.id === 'scrapeData') {
+    scrapeArticles()
+    console.log('hi')
+  }
+
+  if (e.target.id === 'deleteData') {
+    deleteArticles()
+  }
+
+  if (e.target.className === 'btn btn-primary saveArticle') {
+    console.log(e.target.dataset.id)
+    saveArticles(e.target.dataset.id)
+    showArticles()
+  }
+})
---
+++ public/savedHtml/saved.js
@@ -0,0 +1,153 @@
+// function for modal for article comments
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+
+
+//builds the articles
+let buildSaved = articles => {
+  document.getElementById('savedArticles').innerHTML = ''
+  articles.forEach(article => {
+    let artElem = document.createElement('div')
+    artElem.innerHTML = `
+	<div class="card" style="width: auto;">
+  <div class="card-body">
+  <a href="${article.url}" target ="_blank" class="btn btn-primary heading" id="nav">${article.heading}</a>
+    <p class="card-text">${article.summary}</p>
+    <button type="button" class="btn btn-primary comment" data-toggle="modal" data-target="#${article._id}">
+      Article Comment
+    </button>
+ <button class="btn btn-primary delSaved" id="a${article._id}">Delete From Saved</button>
+  </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="${article._id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+  <ul class="list-group" id='r${article._id}'>
+  <li class="list-group-item"></li>
+</ul>
+      <div class="modal-body">
+       <textarea class="form-control" id="t${article._id}" rows="3"></textarea>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary textarea" data-id="t${article._id}" data-dismiss="modal">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+		`
+    document.getElementById('savedArticles').append(artElem)
+  })
+}
+
+// build comment list within a saved article
+
+let buildComments = (comments, id) => {
+
+  document.getElementById(`r${id}`).innerHTML = ''
+  comments.forEach(comment => {
+    let listElem = document.createElement('li')
+    listElem.className = 'list-group-item'
+    // listElem.id = `${comment._id}`
+    listElem.innerHTML = `
+  ${comment.notes}
+  <button type="button" class="btn btn-danger delete" data-id = "${comment._id}" data-dismiss="modal">delete</button>
+  `
+    document.getElementById(`r${id}`).append(listElem)
+
+  })
+}
+
+// show saved articles on saved.HTML
+let savedArticles = () => {
+
+  // let articles = [{2: 2}, {2:  3}]
+  // buildSaved(articles)
+  axios.get('savedArticles')
+    .then(({ data }) => {
+      buildSaved(data)
+    })
+    .catch(e => console.error(e))
+}
+savedArticles()
+
+// grabs comment from textarea
+document.addEventListener('click', e => {
+  if (e.target.className === 'btn btn-primary textarea') {
+    let notesId = e.target.dataset.id
+    let realId = notesId.slice(1)
+    let notes = document.getElementById(notesId).value
+    let comment = {
+      notes: notes,
+      parent: realId
+    }
+    console.log(comment)
+    addNote(comment)
+
+    document.getElementById(notesId).value = ''
+
+  }
+  // Event to show comments when article comment is clicked
+  if (e.target.className === 'btn btn-primary comment') {
+    let datasetId = e.target.dataset.target
+    let commentId = datasetId.slice(1)
+    showNote(commentId)
+  }
+
+  if (e.target.className === 'btn btn-danger delete') {
+    let datasetId = e.target.dataset.id
+    deleteNote(datasetId)
+
+  }
+
+  if (e.target.className === 'btn btn-primary delSaved') {
+    let articleId = e.target.id.slice(1)
+    deleteArticle(articleId)
+  }
+
+})
+
+// add comment to article
+let addNote = notes => {
+  axios.post('/comments', notes)
+    .then(() => {
+      console.log("sucess")
+    })
+    .catch(e => console.error(e))
+}
+
+// delete comment
+let deleteNote = deleteId => {
+  axios.delete(`/comments/${deleteId}`)
+    .then(() => {
+
+    })
+    .catch(e => console.error(e))
+}
+
+//show comment(s) on article
+let showNote = commentId => {
+  axios.get(`/comments/${commentId}`)
+    .then(({ data }) => {
+      buildComments(data, commentId)
+    })
+    .catch(e => console.error(e))
+}
+
+// delete article from saved 
+let deleteArticle = deleteArt => {
+  axios.delete(`/article/${deleteArt}`)
+    .then(() => {
+      savedArticles()
+    })
+    .catch(e => console.error(e))
+}
